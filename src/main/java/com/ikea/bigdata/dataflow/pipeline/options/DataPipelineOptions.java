c4471dc6dc98f245085e10ec7b7382b8a87b3322
Dnyanesh Barkade <dnyaneshlb@github.com>
2019-11-13 15:43:39 +0530

    Added new pipeline options

--- src/main/java/com/ikea/bigdata/dataflow/pipeline/options/DataPipelineOptions.java
+++ src/main/java/com/ikea/bigdata/dataflow/pipeline/options/DataPipelineOptions.java
@@ -1,6 +1,7 @@
 package com.ikea.bigdata.dataflow.pipeline.options;
 
 import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
+import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.Validation;
 
@@ -9,36 +10,41 @@ public interface DataPipelineOptions extends GcpOptions {
     @Validation.Required
     @Description("Topic to read payload from ingestion queue")
     String getOrderEventsTopic();
-
     void setOrderEventsTopic(String orderEventsTopic);
 
+
+    @Default.Integer(2)
+    @Description("Size of window for emitting results")
+    int getWindowSize();
+    void setWindowSize(int windowSize);
+
+    @Default.Integer(10)
+    @Description("Minimum elements in a window before triggering transformations")
+    int getLeastElementsInWindow();
+    void setLeastElementsInWindow(int leastElementsInWindow);
 
     @Validation.Required
     @Description("Subscription from which payload is read")
     String getEventSubscription();
-
     void setEventSubscription(String eventSubscription);
 
     @Validation.Required
     @Description("Database URL command line argument.")
     String getDatabaseURL();
-
     void setDatabaseURL(String databaseURL);
 
     @Validation.Required
     @Description("Database USERNAME command line argument.")
     String getDatabaseUserName();
-
     void setDatabaseUserName(String databaseUserName);
 
     @Validation.Required
     @Description("Database PASSWORD command line argument.")
     String getDatabasePassword();
-
     void setDatabasePassword(String databasePassword);
 
-    @Description("Failure Log Topic")
+    @Validation.Required
+    @Description("Pubsub for events whose processing failed.")
     String getFailureDataTopic();
-
     void setFailureDataTopic(String topic);
 }
