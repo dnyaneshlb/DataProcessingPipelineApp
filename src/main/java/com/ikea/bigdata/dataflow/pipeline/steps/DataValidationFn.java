3b5a03a8507a9d2f104e9b08e8846821187ed773
Dnyanesh Barkade <dnyaneshlb@github.com>
2019-11-18 17:43:19 +0530

    Data validation and tuple processing

---
+++ src/main/java/com/ikea/bigdata/dataflow/pipeline/steps/DataValidationFn.java
@@ -0,0 +1,26 @@
+package com.ikea.bigdata.dataflow.pipeline.steps;
+
+import com.ikea.bigdata.common.Constants;
+import com.ikea.bigdata.protos.OrderProtos;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
+@Slf4j
+public class DataValidationFn extends DoFn<OrderProtos.Order, OrderProtos.Order> {
+    @ProcessElement
+    public void processElement(@Element OrderProtos.Order order, ProcessContext out) {
+        if (Objects.nonNull(order)) {
+            if (!StringUtils.isEmpty(order.getId()) && !StringUtils.isEmpty(order.getShippingaddress())
+                    && !StringUtils.isEmpty(order.getEmail()) && order.getCost() > 0) {
+                out.output(Constants.VALID_DATA, order);
+            } else {
+                log.error("Data received is corrupt. Sending it back to dead letter queue for reprocessing");
+                System.out.println("Data received is corrupt. Sending it back to dead letter queue for reprocessing");
+                out.output(Constants.INVALID_DATA, order);
+            }
+        }
+    }
+}
--- src/main/java/com/ikea/bigdata/dataflow/pipeline/steps/ListConverterFn.java
+++ src/main/java/com/ikea/bigdata/dataflow/pipeline/steps/ListConverterFn.java
@@ -1,17 +1,23 @@
 package com.ikea.bigdata.dataflow.pipeline.steps;
 
+import com.ikea.bigdata.common.Constants;
 import com.ikea.bigdata.protos.OrderProtos;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.beam.sdk.transforms.DoFn;
 
+@Slf4j
 public class ListConverterFn extends DoFn<OrderProtos.Order, String> {
     @ProcessElement
-    public void processElement(@Element OrderProtos.Order order, OutputReceiver<String> out){
-        System.out.println("processing order with mail id as "+ order.getEmail());
+    public void processElement(@Element OrderProtos.Order order, OutputReceiver<String> out) {
+        log.debug("Creating comma separated list of order attributes");
         StringBuilder orderString = new StringBuilder();
         orderString.append(order.getId())
+                .append(Constants.SEPARATOR_COMMA)
                 .append(order.getShippingaddress())
+                .append(Constants.SEPARATOR_COMMA)
                 .append(order.getCost())
-                .append(order.getEmail()).toString();
+                .append(Constants.SEPARATOR_COMMA)
+                .append(order.getEmail());
         out.output(orderString.toString());
     }
 }
